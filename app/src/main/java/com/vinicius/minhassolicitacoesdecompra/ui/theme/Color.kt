eb3bfd63da91bd0a5f16ee87532dadf8ef47e3c6
Vinicius Matos <viniciusmatozdeveloper@gmail.com>
2023-04-24 18:45:57 -0400

    Initial commit

---
+++ app/src/main/java/com/vinicius/minhassolicitacoesdecompra/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.vinicius.minhassolicitacoesdecompra.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val YellowDefault = Color(0xFFFED32C)
+val GreyTextBox = Color(0xFF7D7F82)
+val GreyBox = Color(0xFF202329)
+val DarkBackground = Color(0xFF0D1318)
+val RedCircle = Color (0xFFEC6A6A)
+val BlueCircle = Color(0xFF454AC7)
+val GreenCircle = Color(0xFF489C4B)
+val GreyDefalt = Color(0xFF2B3138)
---
+++ app/src/main/java/com/vinicius/minhassolicitacoesdecompra/view/Home.kt
@@ -0,0 +1,197 @@
+package com.vinicius.minhassolicitacoesdecompra.view
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.vinicius.minhassolicitacoesdecompra.R
+import com.vinicius.minhassolicitacoesdecompra.ui.theme.BlueCircle
+import com.vinicius.minhassolicitacoesdecompra.ui.theme.DarkBackground
+import com.vinicius.minhassolicitacoesdecompra.ui.theme.GreenCircle
+import com.vinicius.minhassolicitacoesdecompra.ui.theme.GreyBox
+import com.vinicius.minhassolicitacoesdecompra.ui.theme.GreyTextBox
+import com.vinicius.minhassolicitacoesdecompra.ui.theme.RedCircle
+import com.vinicius.minhassolicitacoesdecompra.ui.theme.YellowDefault
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Home(){
+    var search by remember {
+        mutableStateOf("")
+    }
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = DarkBackground
+    ){
+        Column{
+            Card(
+                modifier = Modifier
+                    .padding(top = 20.dp, start = 15.dp, end = 15.dp, bottom = 10.dp)
+                    .fillMaxWidth()
+                    .height(150.dp),
+                shape = ShapeDefaults.Medium,
+                colors = CardDefaults.cardColors(GreyBox),
+                elevation = CardDefaults.cardElevation(defaultElevation = 7.dp)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .fillMaxWidth(),
+                    text = "Solicitações de compra",
+                    color = YellowDefault,
+                    fontSize = 21.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                )
+                Row (
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
+                ){
+                    Column (modifier = Modifier
+                        .padding(top = 8.dp, start = 10.dp, end = 10.dp)
+                        .fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Canvas(modifier = Modifier
+                            .size(55.dp),
+                            onDraw = {
+                                drawCircle(color = RedCircle)
+                            } )
+                        Text(
+                            text = "SC em andamento",
+                            modifier = Modifier
+                                .width(70.dp)
+                                .padding(top = 2.dp, bottom = 2.dp),
+                            fontWeight = FontWeight.Medium,
+                            fontStyle = FontStyle.Normal,
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    Column (modifier = Modifier
+                        .padding(top = 8.dp, start = 10.dp, end = 10.dp)
+                        .fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Canvas(modifier = Modifier
+                            .size(55.dp),
+                            onDraw = {
+                                drawCircle(BlueCircle)
+                            } )
+                        Text(
+                            text = "PC em aprovação",
+                            modifier = Modifier
+                                .width(65.dp)
+                                .padding(top = 2.dp, bottom = 2.dp),
+                            fontWeight = FontWeight.Medium,
+                            fontStyle = FontStyle.Normal,
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    Column (modifier = Modifier
+                        .padding(top = 8.dp, start = 10.dp, end = 10.dp)
+                        .fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Canvas(modifier = Modifier
+                            .size(55.dp),
+                            onDraw = {
+                                drawCircle(color = GreenCircle)
+                            } )
+                        Text(
+                            text = "Aguardando Entrega",
+                            modifier = Modifier
+                                .width(75.dp)
+                                .padding(top = 2.dp, bottom = 2.dp),
+                            fontWeight = FontWeight.Medium,
+                            fontStyle = FontStyle.Normal,
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            }
+            OutlinedTextField(
+                value = search,
+                onValueChange ={search = it},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(55.dp)
+                    .padding(start = 15.dp, end = 15.dp),
+                singleLine = true,
+                label = {
+                    Text(text = "Pesquisar")},
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    textColor = Color.White,
+                    disabledTextColor = GreyTextBox,
+                    containerColor = GreyBox,
+                    unfocusedLabelColor = GreyTextBox,
+                    focusedLabelColor = YellowDefault,
+                    focusedBorderColor = YellowDefault),
+                leadingIcon ={
+                    Icon(imageVector = Icons.Rounded.Search,
+                        contentDescription ="search icon" )
+                },
+                trailingIcon ={
+                    IconButton(onClick = {})
+                    {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_filter),
+                            contentDescription = "Icon filter",
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
+@Composable
+@Preview
+fun Preview(
+) {
+    Home()
+}
